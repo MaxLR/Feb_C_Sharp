21cb9dcc325c89db472962f51268f2e1750d83ec
Narciso Lobo <44825108+narcisolobo@users.noreply.github.com>
2023-02-13 08:56:53 -0800

    added w2d1 algos

---
+++ algos/data_structures/BinarySearchTree/simple.js
@@ -0,0 +1,31 @@
+class BSTNode {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  min(current = this.root) {
+    if (!current) return null;
+    while (current.left) {
+      current = current.left;
+    }
+    return current.data;
+  }
+}
+
+const threeLevelTree = new BinarySearchTree();
+threeLevelTree.root = new BSTNode(10);
+threeLevelTree.root.left = new BSTNode(5);
+threeLevelTree.root.left.left = new BSTNode(2);
+threeLevelTree.root.left.right = new BSTNode(6);
+threeLevelTree.root.right = new BSTNode(15);
+threeLevelTree.root.right.left = new BSTNode(13);
+
+console.log(threeLevelTree.min())
